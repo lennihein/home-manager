a75e8f146d608bf94160410b5d5399e35b4a2cb5
Lenni Hein <git@lennihein.com>
2025-01-25 19:43:20 +0100

    added nix run func

--- common/shell.nix
+++ common/shell.nix
@@ -10,10 +10,11 @@
 		functions = {
 			pls = "doas (which $argv[1]) $argv[2..-1]";
 			squash = "git reset (git commit-tree 'HEAD^{tree}' -m $argv)";
-			run =''gcc $argv -o run_tmp && ./run_tmp; set ret_code $status || echo && rm run_tmp && echo "exit code: "$ret_code'';
+			crun =''gcc $argv -o run_tmp && ./run_tmp; set ret_code $status || echo && rm run_tmp && echo "exit code: "$ret_code'';
 			gitignore = ''echo "$argv" >> .gitignore'';
 			gtree = "git log --all --decorate --graph --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)';";
 			detached = "$argv .&> /dev/null & && disown";
+            run = "nix run nixpkgs#$argv[1] $argv[2..-1]";
 		};
 
 		shellAbbrs = {
@@ -22,7 +23,7 @@
 			rm = "rip";
 			rm_ = "rm";
 			py = "python3";
-			gitclean = "git clean -d -i -x";
+            gitclean = "git clean -d -i -x";
 		};
 	};
 
