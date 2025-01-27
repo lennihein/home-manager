ba9213f72f29f0d70413d099043eab461028d794
Lenni Hein <git@lennihein.com>
2025-01-27 17:33:05 +0100

    moved stateversion in it's own file. could override if needed

--- configs/archwsl.nix
+++ configs/archwsl.nix
@@ -1,11 +1,14 @@
 { config, pkgs, lib, ... }:
 {
 
-	home.stateVersion = "24.11";
-
 	imports = [
 
 	];
   
-  home.file.".config/neofetch/config.conf".source = lib.mkForce ../files/neofetch_archwsl.conf;
+	home.file.".config/neofetch/config.conf".source = lib.mkForce ( pkgs.writeText "neofetch_archwsl.conf" ''
+	${builtins.readFile ../files/neofetch_wsl.conf}
+
+	ascii_distro="arch_small"
+
+	'' ) ;
 }
--- configs/wsl.nix
+++ configs/wsl.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 {
 
-	home.stateVersion = "24.11";
-
 	imports = [
 
 	];
