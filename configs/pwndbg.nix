9af9b331e4d208fbfe0c9c747f74a5e79bc3f851
Lenni Hein <git@lennihein.com>
2025-02-16 15:36:12 +0100

    .

--- configs/pwndbg.nix
+++ configs/pwndbg.nix
@@ -1,31 +1,12 @@
-{
-  config, lib, pkgs,
-  ...
-}: let
-  inherit (lib) mkEnableOption mkIf mkOption types;
-in {
-  options.programs.pwndbg = {
-    enable = mkEnableOption "pwndbg";
-
-    package = mkOption {
-      type = types.package;
-      default = pkgs.callPackage
-        (pkgs.fetchFromGitHub {
+{ config, lib, pkgs, ... }:
+  
+  let
+    pwndbg-flake = pkgs.callPackage (pkgs.fetchFromGitHub {
           owner = "pwndbg";
           repo = "pwndbg";
           rev = "master";
           sha256 = "sha256-75C2SswY93LF574xto5m75BpFJVMfoFQMJIP9v261ZQ=";
-        })
-        {};
-      description = "The pwndbg package to use";
-    };
-  };
-
-  config = mkIf config.programs.pwndbg.enable {
-    home.packages = [
-      (pkgs.writeShellScriptBin "pwndbg" ''
-        ${config.programs.pwndbg.package}/bin/pwndbg "$@"
-      '')
-    ];
-  };
-}
+    }) {};
+in {
+    home.packages = [ pwndbg-flake ];
+  }
