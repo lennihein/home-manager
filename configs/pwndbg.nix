0c82185575a1081625c2bb78b0ccba1faec99eae
Lenni Hein <git@lennihein.com>
2025-02-16 15:29:06 +0100

    .

--- configs/pwndbg.nix
+++ configs/pwndbg.nix
@@ -3,13 +3,24 @@
 
 let
   pwndbg = import (builtins.fetchTarball {
-    url = "https://github.com/pwndbg/pwndbg/archive/refs/tags/2025.01.20.tar.gz";
-    sha256 = "0hgzq6fay13c3b06yxl9c3wmly0s48dhp6z25yzwvakxw03k5anr"; # Replace with the actual hash
+    url = "https://github.com/pwndbg/pwndbg/archive/refs/heads/master.tar.gz";
   }) { };
 in
 {
+  home.file.".gdbinit" = {
+    source = ''
+      source ${pwndbg}/gdbinit.py
+    '';
+    mode = "0644";
+  };
+
+  environment.variables.GDBINIT = "${pkgs.writeText "gdbinit" ''
+    source ${pwndbg}/gdbinit.py
+  ''}/gdbinit";
+
   home.packages = with pkgs; [
     gdb
+    python3
     pwndbg
   ];
 }
