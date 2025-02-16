139616cbf787a0b2d87344c6c1aee4530dbba63a
Lenni Hein <git@lennihein.com>
2025-02-16 15:19:38 +0100

    .

--- configs/pwndbg.nix
+++ configs/pwndbg.nix
@@ -1,13 +1,30 @@
-{ config, lib, pkgs, ... }:
+# pwndbg.nix
+{ config, pkgs, ... }:
 
 let
-  pwndbg-flake = pkgs.callPackage (pkgs.fetchFromGitHub {
-    owner = "pwndbg";
-    repo = "pwndbg";
-    rev = "main";
-    sha256 = lib.fakeHash;
-  }) {};
+  pwndbg = import (builtins.fetchTarball {
+    url = "https://github.com/pwndbg/pwndbg/archive/refs/tags/2025.01.20.tar.gz;
+    sha256 = "0hgzq6fay13c3b06yxl9c3wmly0s48dhp6z25yzwvakxw03k5anr"; # Replace with the actual hash
+  }) { };
+in
+{
+  home.file.".gdbinit" = {
+    source = ''
+      source ${pwndbg}/gdbinit.py
+    '';
+    mode = "0644";
+  };
 
-in {
-  home.packages = [ pwndbg-flake ];
+  home-manager.users.lenni = {
+    environment.variables = {
+      GDBINIT = "${pkgs.writeText "gdbinit" ''
+        source ${pwndbg}/gdbinit.py
+      ''}/gdbinit";
+    };
+  };
+
+  home.packages = with pkgs; [
+    gdb
+    pwndbg
+  ];
 }
