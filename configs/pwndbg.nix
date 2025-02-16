599ae3fff4064f0a0fc6b02bbc9719f3321d2597
Lenni Hein <git@lennihein.com>
2025-02-16 15:21:39 +0100

    .

--- configs/pwndbg.nix
+++ configs/pwndbg.nix
@@ -3,7 +3,7 @@
 
 let
   pwndbg = import (builtins.fetchTarball {
-    url = "https://github.com/pwndbg/pwndbg/archive/refs/tags/2025.01.20.tar.gz;
+    url = "https://github.com/pwndbg/pwndbg/archive/refs/tags/2025.01.20.tar.gz";
     sha256 = "0hgzq6fay13c3b06yxl9c3wmly0s48dhp6z25yzwvakxw03k5anr"; # Replace with the actual hash
   }) { };
 in
