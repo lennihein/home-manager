02a3c7a4874a86f7362d8c0ce5817b8371d5222b
Lenni Hein <git@lennihein.com>
2025-05-26 09:22:44 +0200

    fix wsl display

--- files/wsl.fish
+++ files/wsl.fish
@@ -1,5 +1,5 @@
 # export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-export DISPLAY=172.19.32.1:0.0
+export DISPLAY=172.17.0.1:0.0
 export GDK_BACKEND=x11
 
 abbr --add code "/mnt/c/Users/Lenni/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
