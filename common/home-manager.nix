d453539e666a9173b8d7a774abb474da1e2f4fc3
Lenni Hein <git@lennihein.com>
2025-09-01 13:48:32 +0200

    disable Home Manager manual outputs (html/manpages)

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -1,4 +1,10 @@
 { config, pkgs, ... }:
 {
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
+
+  # Disable Home Manager manual outputs to speed builds and avoid options.json warning
+  manual = {
+    html.enable = false;
+    manpages.enable = false;
+  };
 }
