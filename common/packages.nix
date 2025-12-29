8ef1b59ec2b1f01d5c71aa1a6ffe20f8f7c3dc34
Lenni Hein <git@lennihein.com>
2025-12-29 13:27:41 +0100

    add todoist cli to common packages

--- common/packages.nix
+++ common/packages.nix
@@ -4,7 +4,7 @@
     # TUI
     helix gitui ranger gdu bottom fastfetch
     # CLI
-    bat eza rm-improved duf xcp less codex gemini-cli gh
+    bat eza rm-improved duf xcp less codex gemini-cli gh todoist
     # network
     curl wget
   ];
